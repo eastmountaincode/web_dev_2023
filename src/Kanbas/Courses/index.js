e9b3c18110fbf1c9791b302c497a63fa23b26e8a
andrewboylan <andreweboylan@gmail.com>
2023-11-15 11:59:52 -0600

    env variable for Courses

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -15,7 +15,7 @@ import { useParams } from "react-router-dom";
 
 
 function Courses() {
-    const API_BASE_URL = "http://localhost:4000/api/courses";
+    const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000/api/courses";
 
     const { courseId } = useParams();
     const [course, setCourse] = useState({});
